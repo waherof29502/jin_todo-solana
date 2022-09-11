5bcd93280df1d0d51a105e745b1c5048c1444476
JinPing <39645652+waherof29502@users.noreply.github.com>
2022-09-11 23:35:23 +0800

    Finished todoApp

---
+++ app/components/WalletConnectProvider.js
@@ -0,0 +1,51 @@
+import { useMemo } from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+export const WalletConnectProvider = ({ children }) => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => {
+    if (network === WalletAdapterNetwork.Devnet) {
+      return 'https://late-wild-scion.solana-devnet.discover.quiknode.pro/a93715cc2505dae5b4b18e21f7c07f5d4e029978/';
+    }
+
+    return clusterApiUrl(network);
+  }, [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      //   new GlowWalletAdapter(),
+      //   new SlopeWalletAdapter(),
+      //   new SolflareWalletAdapter({ network }),
+      //   new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,5 @@
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey } from '@solana/web3.js';
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("EMRELEkPnwDNsuyHmYZYmMroJ69FpdAfSgDHrTxEYrq3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey(
+  'FvniBrz7VCq9d2SobAGzrqXMUApS2Jrtn7a4voiYuh7N'
+);
